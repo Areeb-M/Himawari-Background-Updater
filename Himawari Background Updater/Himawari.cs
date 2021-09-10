5ebf58a35afc5536dbf10a25822a23edaa839b25
Areeb Mohammed <areebmohammed11@gmail.com>
2021-09-10 16:29:31 -0500

    Initial commit

---
+++ Himawari Background Updater/Himawari.cs
@@ -0,0 +1,134 @@
+﻿using Himawari_Background_Updater.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Himawari_Background_Updater
+{
+    class Himawari : ApplicationContext
+    {
+        Container components;
+        NotifyIcon notifyIcon;
+
+        ToolStripMenuItem refresh;
+        ToolStripMenuItem status;
+        ToolStripMenuItem updateTime;
+        ToolStripSeparator toolStripSeparator;
+        ToolStripMenuItem exit;
+
+        ThreadStart childref;
+        Thread childThread;
+
+        System.Windows.Forms.Timer timer;
+
+        bool refreshing = false;
+
+        public Himawari()
+        {
+            InitializeContext();
+            InitializeTimer();
+            Refresh_Background(null, null);
+        }
+
+        private void InitializeContext()
+        {
+            components = new Container();
+            notifyIcon = new NotifyIcon(components)
+                {
+                    ContextMenuStrip = new ContextMenuStrip(),
+                    Icon = Resources.AppIcon,
+                    Text = "Himawari",
+                    Visible = true
+                };
+            refresh = new ToolStripMenuItem("Refresh", null, Refresh_Background);
+            status = new ToolStripMenuItem("Disable", null, Enable_Disable_Switch);
+            updateTime = new ToolStripMenuItem("XX:XX XX");
+            toolStripSeparator = new ToolStripSeparator();
+            exit = new ToolStripMenuItem("Exit", null, exitItem_Click);
+
+            notifyIcon.ContextMenuStrip.Items.Add(refresh);
+            notifyIcon.ContextMenuStrip.Items.Add(status);
+            notifyIcon.ContextMenuStrip.Items.Add(new ToolStripSeparator());
+            notifyIcon.ContextMenuStrip.Items.Add(new ToolStripMenuItem("Last Updated"));
+            notifyIcon.ContextMenuStrip.Items.Add(updateTime);
+            notifyIcon.ContextMenuStrip.Items.Add(toolStripSeparator);
+            notifyIcon.ContextMenuStrip.Items.Add(exit);
+        }
+
+        private void InitializeTimer()
+        {
+            timer = new System.Windows.Forms.Timer();
+            timer.Interval = 10 * 60 * 1000;
+
+            timer.Tick += Refresh_Background;
+            timer.Enabled = true;
+        }
+
+        private void Refresh_Background(object sender, EventArgs e)
+        {
+            if (refreshing)
+                return;
+            childref = new ThreadStart(Refresh_Thread);
+            childThread = new Thread(childref);
+            childThread.Start();
+        }
+
+        private void Refresh_Thread()
+        {
+            try
+            {
+                refreshing = true;
+
+                WebRequest request = WebRequest.Create("http://rammb.cira.colostate.edu/ramsdis/online/images/latest_hi_res/himawari-8/full_disk_ahi_true_color.jpg");
+                WebResponse response = request.GetResponse();
+
+                Image image = Image.FromStream(response.GetResponseStream());
+                response.Close();
+
+                image.Save("latest.jpg");
+                image.Dispose();
+                WallpaperUpdater.SetWallpaper(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + "\\latest.jpg");
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.StackTrace);
+                refreshing = false;
+            }
+
+            updateTime.Text = DateTime.Now.ToString("h\\:mm tt");
+            refreshing = false;
+        }
+
+        private void Enable_Disable_Switch (object sender, EventArgs e)
+        {
+            if (status.Text == "Disable")
+            {
+                timer.Enabled = false;
+                status.Text = "Enable";
+            } else
+            {
+                timer.Enabled = true;
+                status.Text = "Disable";
+            }
+        }
+
+        private void exitItem_Click(object sender, EventArgs e)
+        {
+            ExitThread();
+        }
+
+        protected override void ExitThreadCore()
+        {
+            notifyIcon.Visible = false;
+            base.ExitThreadCore();
+        }
+    }
+}
---
+++ Himawari Background Updater/WallpaperUpdater.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Himawari_Background_Updater
+{
+    class WallpaperUpdater
+    {
+        public const int SetDesktopWallpaper = 20;
+        public const int UpdateIniFile = 0x01;
+        public const int SendWinIniChange = 0x02;
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        private static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+        public static void SetWallpaper(string path)
+        {
+            SystemParametersInfo(SetDesktopWallpaper, 0, path, UpdateIniFile | SendWinIniChange);
+        }
+    }
+}
